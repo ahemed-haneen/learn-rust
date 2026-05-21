3bdf4f38d293f35bb9762cb0fd63064c03a3a952
Haneen Ahemed A <haneenahemed.a@gmail.com>
2026-05-21 21:44:38 +0000

    push scalar variables lesson

---
+++ rust-scalars/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let a = 5;
+    let b: u8 = 1;
+
+    let x = 2.0;
+    let y: f32 = 3.0;
+
+    let sum = a + b;
+    
+    let difference = x - 1.0;
+
+    let product = 4 * a;
+
+    let quotient = 9.0 / y;
+
+    let remainder = a % b;
+
+    let t = true;
+    let f: bool = false;
+
+    let c = 'c';
+    let d: char = 'a';
+
+    println!("Sum: {}", sum);
+    println!("Difference: {}", difference);
+    println!("Product: {}", product);
+    println!("Quotient: {}", quotient);
+    println!("Remainder: {}", remainder);
+    println!("True: {}", t);
+    println!("False: {}", f);
+    println!("Character c: {}", c);
+    println!("Character d: {}", d);
+
+}
---
+++ rust-variables/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut x: i32 = 1;
+    println!("the value of x is : {}", x);
+
+    x = 2;
+    println!("the value of x is : {}", x);
+
+    let y: bool = true;
+    println!("The value of y is : {}", y);
+
+    let y: bool = false;
+    println!("The value of y is : {}", y);
+
+    const STRING: &str = "hello";
+    println!("The value of the string constant is : {}", STRING);
+}
